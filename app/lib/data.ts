2b96f82d3dc1da278196273185bb1b54e5422fac
yuutaaaa <moniboreychay123@gmail.com>
2023-11-10 10:42:20 +0700

    search

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -13,19 +13,19 @@ import { formatCurrency } from './utils';
 
 export async function fetchRevenue() {
   // Add noStore() here prevent the response from being cached.
-  // This is equivalent to in fetch(..., {cache: 'no-store'}).
+  // This is equivalent to i fnetch(..., {cache: 'no-store'}).
   unstable_noStore();
 
   try {
     // Artificially delay a reponse for demo purposes.
     // Don't do this in real life :)
 
-    console.log('Fetching revenue data...');
-    await new Promise((resolve) => setTimeout(resolve, 3000));
+    // console.log('Fetching revenue data...');
+    // await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
-    console.log('Data fetch complete after 3 seconds.');
+    // console.log('Data fetch complete after 3 seconds.');
 
     return data.rows;
   } catch (error) {
--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -22,7 +22,6 @@ const links = [
 
 export default function NavLinks() {
   const pathname = usePathname();
-
   return (
     <>
       {links.map((link) => {
