e3ce41fe71832fa5d3489aaebc947c040a71a665
yuutaaaa <moniboreychay123@gmail.com>
2023-11-10 11:33:08 +0700

    fixed

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -15,7 +15,7 @@ export default async function Page({
     page?: string;
   }
 }) {
-  const query = searchParams?.a || '';
+  const query = searchParams?.query || '';
   const currentPage = Number(searchParams?.page) || 1;
   const totalPages = await fetchInvoicesPages(query);
 
